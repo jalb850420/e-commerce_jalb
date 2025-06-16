ddf3df4af63c03cd4afff43986d1a8bb3a12b5d4
Joaquin Liñan <joaquin.linanb@pca.edu.co>
2025-06-15 23:03:35 -0500

    modificacion de ruta de imagenes imagenes/

--- src/app.jsx
+++ src/app.jsx
@@ -56,24 +56,24 @@ const App = () => {
       <Header />
       <div className="Contenedor1">
         <div className="ProductosContainer">
-          <Card className="Card" imagen="../imagenes/adidas_samba_hombre/adidas_samba1.jpg" titulo="Adidas Samba Hombre / Ref.1" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/adidas_samba_hombre/adidas_samba2.jpg" titulo="Adidas Samba Hombre / Ref.2" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/adidas_samba_hombre/adidas_samba3.jpg" titulo="Adidas Samba Hombre / Ref.3" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/adidas_samba_hombre/adidas_samba4.jpg" titulo="Adidas Samba Hombre / Ref.4" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/adidas_samba_hombre/adidas_samba5.jpg" titulo="Adidas Samba Hombre / Ref.5" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/adidas_samba_hombre/adidas_samba1.jpg" titulo="Adidas Samba Hombre / Ref.1" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/adidas_samba_hombre/adidas_samba2.jpg" titulo="Adidas Samba Hombre / Ref.2" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/adidas_samba_hombre/adidas_samba3.jpg" titulo="Adidas Samba Hombre / Ref.3" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/adidas_samba_hombre/adidas_samba4.jpg" titulo="Adidas Samba Hombre / Ref.4" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/adidas_samba_hombre/adidas_samba5.jpg" titulo="Adidas Samba Hombre / Ref.5" precio="130000" setCarrito={setCarrito}/>
 
-          <Card className="Card" imagen="../imagenes/DC_Hombre/dc_hombre1.jpg" titulo="DC_Hombre / Ref.1" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/DC_Hombre/dc_hombre2.jpg" titulo="DC_Hombre / Ref.2" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/DC_Hombre/dc_hombre3.jpg" titulo="DC_Hombre / Ref.3" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/DC_Hombre/dc_hombre4.jpg" titulo="DC_Hombre / Ref.4" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/DC_Hombre/dc_hombre5.jpg" titulo="DC_Hombre / Ref.5" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/DC_Hombre/dc_hombre1.jpg" titulo="DC_Hombre / Ref.1" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/DC_Hombre/dc_hombre2.jpg" titulo="DC_Hombre / Ref.2" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/DC_Hombre/dc_hombre3.jpg" titulo="DC_Hombre / Ref.3" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/DC_Hombre/dc_hombre4.jpg" titulo="DC_Hombre / Ref.4" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/DC_Hombre/dc_hombre5.jpg" titulo="DC_Hombre / Ref.5" precio="130000" setCarrito={setCarrito}/>
 
-          <Card className="Card" imagen="../imagenes/nike_sb/nike_sb1.jpg" titulo="Nike sb / Ref.1" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/nike_sb/nike_sb2.jpg" titulo="Nike sb / Ref.2" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/nike_sb/nike_sb3.jpg" titulo="Nike sb / Ref.3" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/nike_sb/nike_sb4.jpg" titulo="Nike sb / Ref.4" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/nike_sb/nike_sb5.jpg" titulo="Nike sb / Ref.5" precio="130000" setCarrito={setCarrito}/>
-          <Card className="Card" imagen="../imagenes/nike_sb/nike_sb6.jpg" titulo="Nike sb / Ref.6" precio="130000" setCarrito={setCarrito}/>                                                        
+          <Card className="Card" imagen="imagenes/nike_sb/nike_sb1.jpg" titulo="Nike sb / Ref.1" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/nike_sb/nike_sb2.jpg" titulo="Nike sb / Ref.2" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/nike_sb/nike_sb3.jpg" titulo="Nike sb / Ref.3" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/nike_sb/nike_sb4.jpg" titulo="Nike sb / Ref.4" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/nike_sb/nike_sb5.jpg" titulo="Nike sb / Ref.5" precio="130000" setCarrito={setCarrito}/>
+          <Card className="Card" imagen="imagenes/nike_sb/nike_sb6.jpg" titulo="Nike sb / Ref.6" precio="130000" setCarrito={setCarrito}/>                                                        
         </div>
         <div className="Carrito">
           <Carrito carrito={carrito} setCarrito={setCarrito} eliminarDelCarrito={eliminarDelCarrito} />
